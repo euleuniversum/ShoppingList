1d28dbb4af9fbf3a493159853d98b933e97b9116
Natalya Koshkina <natalya_draznina@mail.ru>
2021-04-05 23:01:18 +0500

    add total price in table

--- src/conponents/ShoppingTable/ShoppingTable.tsx
+++ src/conponents/ShoppingTable/ShoppingTable.tsx
@@ -1,8 +1,10 @@
 import {Table} from 'antd';
+import styles from './ShoppingTable.module.css';
 import {getColumns} from "./columns";
 import {clearArray} from '../../helpers/app';
 import {IShoppingTableProps, IShoppingTableRow} from "../../interface";
 import {useMemo} from "react";
+import {getTotalPriceElement} from "../../units";
 
 export const ShoppingTable = ({purchases, onEditItem, onDeleteItem}: IShoppingTableProps) => {
     const columns = useMemo(() =>
@@ -21,6 +23,28 @@ export const ShoppingTable = ({purchases, onEditItem, onDeleteItem}: IShoppingTa
     }
 
     return (
-        <Table columns={columns} dataSource={data} onChange={onChange} pagination={false}/>
+        <Table
+            columns={columns}
+            dataSource={data}
+            onChange={onChange}
+            pagination={false}
+            summary={pageData => {
+                let totalPrice = 0;
+                pageData.forEach((purchase) => {
+                    totalPrice += getTotalPriceElement(purchase);
+                });
+
+                return (
+                    <>
+                        <Table.Summary.Row className={styles.tfoot_th}>
+                            <Table.Summary.Cell className={styles.tfoot_td} index={1} colSpan={2}>Итого:</Table.Summary.Cell>
+                            <Table.Summary.Cell className={styles.tfoot_td} index={2} colSpan={4}>
+                                {totalPrice}
+                            </Table.Summary.Cell>
+                        </Table.Summary.Row>
+                    </>
+                );
+            }}
+        />
     );
 }
