3695049a941d92d6f435c89d56073c7e2b2e7c81
Natalya Koshkina <natalya_draznina@mail.ru>
2021-04-20 20:33:33 +0500

    ref headers csv

--- src/containers/DownloadsTableCSVContainer.ts
+++ src/containers/DownloadsTableCSVContainer.ts
@@ -5,13 +5,13 @@ import { IPurchaseItem, IRootStore} from "../interface";
 function getHeaders() {
     return [
         { label: "ID", key: "id" },
+        { label: "Замена для", key: "replacementFor" },
         { label: "Название", key: "title" },
         { label: "Количество", key: "quantity" },
         { label: "Единица измерения", key: "quantityUnit" },
         { label: "Примерная цена", key: "price" },
         { label: "Единица измерения цены", key: "priceUnit" },
         { label: "Где купить", key: "whereBuy" },
-        { label: "Список замен", key: "replacementFor" },
     ];
 }
 
