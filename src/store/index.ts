772e1a587ab5e58edddc392a4e448ea1dd1a4f9f
Natalya Koshkina <natalya_draznina@mail.ru>
2021-04-28 15:29:37 +0500

    add localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,4 +3,12 @@ import {rootReducer} from "../reducers/root/rootReducer";
 import {logger} from "redux-logger";
 import {composeWithDevTools} from "redux-devtools-extension";
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+export const store = createStore(
+    rootReducer,
+    (localStorage['redux-store'] ? JSON.parse(localStorage['redux-store']) : undefined),
+    composeWithDevTools(applyMiddleware(logger))
+);
+
+store.subscribe(() => {
+    localStorage['redux-store'] = JSON.stringify((store.getState()))
+});
